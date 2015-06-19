a61de0e8c6623369e84c59aef41dfb196f787692
Bence Kiglics <bence.kiglics@prezi.com>
2015-06-19 09:34:12 +0200

    Fixed assert in test

--- languages/nodejs/template/test/gol_test.js
+++ languages/nodejs/template/test/gol_test.js
@@ -4,7 +4,7 @@ var gol    = require('./../src/gol'),
 describe("Game of Life", function() {
     describe("nothing", function() {
         it("should do nothing", function() {
-            assert.equals(gol.testMethod(), 1);
+            assert.equal(gol.testMethod(), 1);
         });
     });
 });
