e7e13e4bf2fe652b2d88dc7880096fc61884d585
Bence Kiglics <bence.kiglics@prezi.com>
2015-06-19 21:27:13 +0200

    Added language python

---
+++ languages/python/check.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env false
+
+function python_available() {
+    which python
+    return $?
+}
---
+++ languages/python/template/gol_test.py
@@ -0,0 +1,7 @@
+import gol
+import unittest
+
+class GolTest(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(1, gol.method())
+
